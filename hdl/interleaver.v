45f368b7d410a2436291900ee25d1ec6295c4eb1
Brett Witherspoon <spoonb@cdspooner.com>
2014-07-03 09:57:19 -0500

    hdl: interleaver: almost complete

--- hdl/interleaver.v
+++ hdl/interleaver.v
@@ -16,7 +16,6 @@
  * i = s * floor(j/s) + (j + floor(16*j/N_cpbs)) mod s
  * k = 16 * i - (N_cbps - 1) * floor(16*i/N_cbps)
  */
-
 module interleaver
     (input aclk,
      input aresetn,
@@ -59,209 +58,207 @@ module interleaver
     //    end
     //endfunction
 
-    wire s_handshake = s_axis_tvalid & s_axis_tready;
     wire m_handshake = m_axis_tvalid & m_axis_tready;
-    reg [1:0] m_addr;
-    wire [7:0] m_dout [3:0];
 
-    assign m_dout[3] = fifo_dout[31:24];
-    assign m_dout[2] = fifo_dout[23:16];
-    assign m_dout[1] = fifo_dout[15:8];
-    assign m_dout[0] = fifo_dout[7:0];
-
-    reg [35:0] fifo_ram [511:0];
+    // FIFO
+    reg [35:0] fifo [511:0];
     reg [8:0] fifo_waddr;
     reg [8:0] fifo_raddr;
     reg [35:0] fifo_dout;
-    reg fifo_rden;
-    wire fifo_full;
-    wire fifo_empty;
+    reg [1:0] fifo_state;
+    // FIXME
+    wire fifo_ren = m_handshake && ~fifo_empty;
+    wire fifo_wen = ctl_state == WRITE && ~fifo_full;
+    wire fifo_empty = fifo_raddr == fifo_waddr;
+    reg fifo_full;
 
-    reg [3:0] p_rate;
+    // Input block
+    reg [7:0] block [35:0];
+    reg [3:0] block_rate;
+    reg [5:0] block_waddr;
+    reg [5:0] block_raddr;
+    wire [287:0] block_net = {block[35], block[34], block[33], block[32],
+                              block[31], block[30], block[29], block[28],
+                              block[27], block[26], block[25], block[24],
+                              block[23], block[22], block[21], block[20],
+                              block[19], block[18], block[17], block[16],
+                              block[15], block[14], block[13], block[12],
+                              block[11], block[10], block[ 9], block[ 8],
+                              block[ 7], block[ 6], block[ 5], block[ 4],
+                              block[ 3], block[ 2], block[ 1], block[ 0]};
 
-    reg valid_int;
-    reg ready_int;
+    wire is_bpsk_rate = block_rate == `RATE_6M || block_rate == `RATE_9M;
+    wire is_qpsk_rate = block_rate == `RATE_12M || block_rate == `RATE_18M;
+    wire is_qam16_rate = block_rate == `RATE_24M || block_rate == `RATE_36M;
+    wire is_qam64_rate = block_rate == `RATE_48M || block_rate == `RATE_54M;
 
-    // Distributed RAM for permutation
-    reg [7:0] p_ram [35:0];
-    reg [5:0] p_ram_ra;
-    reg [5:0] p_ram_wa;
+    wire block_bpsk_end  = is_bpsk_rate && block_waddr == 5;
+    wire block_qpsk_end  = is_qpsk_rate && block_waddr == 12;
+    wire block_qam16_end = is_qam16_rate && block_waddr == 23;
+    wire block_qam64_end = is_qam64_rate && block_waddr == 35;
 
-    reg [15:0] p_bpsk [2:0];
-    reg [31:0] p_qpsk [2:0];
-    reg [31:0] p_qam16 [5:0];
-    reg [31:0] p_qam64 [8:0];
+    wire block_end = block_qam64_end || block_qam16_end ||
+                     block_qpsk_end  || block_bpsk_end;
 
-    reg [31:0] p_dout [8:0];
-    reg [3:0] p_dout_addr;
+    // Interleaving for each block size
+    reg [15:0] inter_bpsk [2:0];
+    reg [31:0] inter_qpsk [2:0];
+    reg [31:0] inter_qam16 [5:0];
+    reg [31:0] inter_qam64 [8:0];
 
-    // Concatenate distributed RAM read ports into a single bus
-    wire [287:0] p_ram_net = {p_ram[35], p_ram[34], p_ram[33], p_ram[32],
-                             p_ram[31], p_ram[30], p_ram[29], p_ram[28],
-                             p_ram[27], p_ram[26], p_ram[25], p_ram[24],
-                             p_ram[23], p_ram[22], p_ram[21], p_ram[20],
-                             p_ram[19], p_ram[18], p_ram[17], p_ram[16],
-                             p_ram[15], p_ram[14], p_ram[13], p_ram[12],
-                             p_ram[11], p_ram[10], p_ram[ 9], p_ram[ 8],
-                             p_ram[ 7], p_ram[ 6], p_ram[ 5], p_ram[ 4],
-                             p_ram[ 3], p_ram[ 2], p_ram[ 1], p_ram[ 0]};
+    wire [CBPS_BPSK-1:0] inter_bpsk_net = {inter_bpsk[2], inter_bpsk[1],
+                                           inter_bpsk[0]};
 
-    // Permutation buses
-    wire [CBPS_BPSK-1:0] p_bpsk_net = {p_bpsk[2], p_bpsk[1], p_bpsk[0]};
-    wire [CBPS_QPSK-1:0] p_qpsk_net = {p_qpsk[2], p_qpsk[1], p_qpsk[0]};
-    wire [CBPS_QAM16-1:0] p_qam16_net = {p_qam16[5], p_qam16[4], p_qam16[3],
-                                         p_qam16[2], p_qam16[1], p_qam16[0]};
-    wire [CBPS_QAM64-1:0] p_qam64_net = {p_qam64[8], p_qam64[7], p_qam64[6],
-                                         p_qam64[5], p_qam64[4], p_qam64[3],
-                                         p_qam64[2], p_qam64[1], p_qam64[0]};
+    wire [CBPS_QPSK-1:0] inter_qpsk_net = {inter_qpsk[2], inter_qpsk[1],
+                                           inter_qpsk[0]};
+
+    wire [CBPS_QAM16-1:0] inter_qam16_net = {inter_qam16[5], inter_qam16[4],
+                                             inter_qam16[3], inter_qam16[2],
+                                             inter_qam16[1], inter_qam16[0]};
+
+    wire [CBPS_QAM64-1:0] inter_qam64_net = {inter_qam64[8], inter_qam64[7],
+                                             inter_qam64[6], inter_qam64[5],
+                                             inter_qam64[4], inter_qam64[3],
+                                             inter_qam64[2], inter_qam64[1],
+                                             inter_qam64[0]};
 
     genvar j;
     for (j = 0; j < CBPS_BPSK; j = j + 1) begin : gen_bpsk
-        assign p_bpsk_net[j] = p_ram_net[permute(j, CBPS_BPSK)];
+        assign inter_bpsk_net[j] = block_net[permute(j, CBPS_BPSK)];
     end
     for (j = 0; j < CBPS_QPSK; j = j + 1) begin : gen_qpsk
-        assign p_qpsk_net[j] = p_ram_net[permute(j, CBPS_QPSK)];
+        assign inter_qpsk_net[j] = block_net[permute(j, CBPS_QPSK)];
     end
-    for (j = 0; j < CBPS_QAM16; j = j + 1) begin : gen_16qam
-        assign p_qam16_net[j] = p_ram_net[permute(j, CBPS_QAM16)];
+    for (j = 0; j < CBPS_QAM16; j = j + 1) begin : gen_qam16
+        assign inter_qam16_net[j] = block_net[permute(j, CBPS_QAM16)];
     end
-    for (j = 0; j < CBPS_QAM64; j = j + 1) begin : gen_64qam
-        assign p_qam64_net[j] = p_ram_net[permute(j, CBPS_QAM64)];
+    for (j = 0; j < CBPS_QAM64; j = j + 1) begin : gen_qam64
+        assign inter_qam64_net[j] = block_net[permute(j, CBPS_QAM64)];
     end
 
-    // Mux
+    wire inter_bpsk_end = is_bpsk_rate && inter_raddr == 1;
+    wire inter_qpsk_end = is_qpsk_rate && inter_raddr == 2;
+    wire inter_qam16_end = is_qam16_rate && inter_raddr == 5;
+    wire inter_qam64_end = is_qam64_rate && inter_raddr == 8;
+
+    wire inter_end = inter_qam64_end || inter_qam16_end ||
+                     inter_qpsk_end  || inter_bpsk_end;
+
+    // Interleaved data
+    reg [31:0] inter [8:0];
+    reg [3:0] inter_raddr;
+
+    // Controller state
+    reg ctl_state;
+
+    localparam READ = 0;
+    localparam WRITE = 1;
+
+    // Controller
+    always @(posedge aclk)
+        if (~aresetn) begin
+            ctl_state <= READ;
+            block_waddr <= 0;
+            inter_raddr <= 0;
+        end
+        else
+            case (ctl_state)
+                READ: begin
+                    if (s_axis_tvalid) begin
+                        block[block_waddr] <= s_axis_tdata;
+                        if (block_waddr == 0)
+                            block_rate <= s_axis_tuser;
+                        if (block_end) begin
+                            block_waddr <= 0;
+                            ctl_state <= WRITE;
+                        end
+                        else begin
+                            block_waddr <= block_waddr + 1;
+                            ctl_state <= READ;
+                        end
+                    end
+                    else begin
+                        ctl_state <= READ;
+                    end
+                end
+                WRITE: begin
+                    if (~fifo_full) begin
+                        if (inter_end) begin
+                            inter_raddr <= 0;
+                            ctl_state <= READ;
+                        end
+                        else begin
+                            inter_raddr <= inter_raddr + 1;
+                            ctl_state <= WRITE;
+                        end
+                    end
+                    else begin
+                        ctl_state <= WRITE;
+                    end
+                end
+            endcase
+
+    assign s_axis_tready = ctl_state == READ;
+
+    // Interleaving mux
     integer k;
     always@*
-        case (p_rate)
+        case (block_rate)
             `RATE_12M, `RATE_18M: begin
                 for (k = 0; k < 3; k = k + 1)
-                    p_dout[k] = p_qpsk[k];
+                    inter[k] = inter_qpsk[k];
                 for (k = 3; k < 9; k = k + 1)
-                    p_dout[k] = {32{1'bx}};
+                    inter[k] = {32{1'bx}};
             end
             `RATE_24M, `RATE_36M: begin
                 for (k = 0; k < 6; k = k + 1)
-                    p_dout[k] = p_qam16[k];
+                    inter[k] = inter_qam16[k];
                 for (k = 6; k < 9; k = k + 1)
-                    p_dout[k] = {32{1'bx}};
+                    inter[k] = {32{1'bx}};
             end
             `RATE_48M, `RATE_54M: begin
                 for (k = 0; k < 9; k = k + 1)
-                    p_dout[k] = p_qam64[k];
+                    inter[k] = inter_qam64[k];
             end
             default: begin
-                p_dout[0] = {p_bpsk[1], p_bpsk[0]};
-                p_dout[1] = {{16{1'bx}}, p_bpsk[2]};
+                inter[0] = {inter_bpsk[1], inter_bpsk[0]};
+                inter[1] = {{16{1'bx}}, inter_bpsk[2]};
                 for (k = 2; k < 9; k = k + 1)
-                    p_dout[k] = {32{1'bx}};
+                    inter[k] = {32{1'bx}};
             end
         endcase
 
-
     // FIFO
     always @(posedge aclk)
-        if (fifo_rden) begin
-            fifo_dout <= fifo_ram[fifo_raddr];
+        if (~aresetn)
+            fifo_raddr <= 0;
+        else if (fifo_ren) begin
+            fifo_dout <= fifo[fifo_raddr];
             fifo_raddr <= fifo_raddr + 1;
         end
 
     always @(posedge aclk)
-        if (valid_int) begin
-            fifo_ram[fifo_waddr] <= {p_rate, p_dout[p_dout_addr]};
+        if (~aresetn)
+            fifo_waddr <= 0;
+        else if (fifo_wen) begin
+            fifo[fifo_waddr] <= {block_rate, inter[inter_raddr]};
             fifo_waddr <= fifo_waddr + 1;
         end
 
-    assign fifo_full = fifo_waddr == fifo_raddr + 1;
-    assign fifo_empty = fifo_waddr == fifo_raddr;
-
-
     always @(posedge aclk)
         if (~aresetn)
-            p_dout_addr <= 0;
-        else
-            case (p_rate)
-                `RATE_6M, `RATE_9M: if (p_dout_addr == 2) p_dout_addr <= 0;
-                `RATE_12M, `RATE_18M: if (p_dout_addr == 3) p_dout_addr <= 0;
-                `RATE_24M, `RATE_36M: if (p_dout_addr == 6) p_dout_addr <= 0;
-                `RATE_48M, `RATE_54M: if (p_dout_addr == 9) p_dout_addr <= 0;
-            endcase
-
-
-    always @(posedge aclk)
-        if (~aresetn)
-            ready_int <= 0;
-        else if (valid_int & ready_int)
-            ready_int <= 0;
-        else
-            case (p_rate)
-                `RATE_6M, `RATE_9M: if (p_dout_addr == 2) ready_int <= 1;
-                `RATE_12M, `RATE_18M: if (p_dout_addr == 3) ready_int <= 1;
-                `RATE_24M, `RATE_36M: if (p_dout_addr == 6) ready_int <= 1;
-                `RATE_48M, `RATE_54M: if (p_dout_addr == 9) ready_int <= 1;
-            endcase
-
-    // FIXME ???
-    assign s_axis_tready = ready_int;
-
-    // AXI-Stream slave interface
-    always @(posedge aclk) begin
-        if (~aresetn)
-            p_ram_wa <= 0;
-        else if (ready_int) begin
-            if (valid_int)
-                valid_int <= 0;
-
-            if (s_handshake) begin
-                p_ram[p_ram_wa] <= s_axis_tdata;
-                p_ram_wa <= p_ram_wa + 1;
-
-                if (p_ram_wa == 0)
-                    p_rate <= s_axis_tuser;
-                else
-                    case (p_rate)
-                        `RATE_6M, `RATE_9M:
-                            if (p_ram_wa == 5) begin
-                                p_ram_wa <= 0;
-                                valid_int <= 1;
-                            end
-                        `RATE_12M, `RATE_18M:
-                            if (p_ram_wa == 12) begin
-                                p_ram_wa <= 0;
-                                valid_int <= 1;
-                            end
-                        `RATE_24M, `RATE_36M:
-                            if (p_ram_wa == 23) begin
-                                p_ram_wa <= 0;
-                                valid_int <= 1;
-                            end
-                        `RATE_48M, `RATE_54M:
-                            if (p_ram_wa == 35) begin
-                                p_ram_wa <= 0;
-                                valid_int <= 1;
-                            end
-                    endcase
-            end
-        end
-    end
+            fifo_full <= 0;
+        else if (fifo_ren && ~fifo_wen)
+            fifo_full <= 0;
+        else if (fifo_wen && ~fifo_ren && fifo_waddr + 2 == fifo_raddr)
+            fifo_full <= 1;
 
     // AXI-Stream master interface
     always @(posedge aclk) begin
         if (~aresetn) begin
-            m_addr <= 0;
             m_axis_tvalid <= 0;
         end
         else if (m_handshake) begin
-            if (m_addr == 3) begin
-                if (fifo_full)
-                    m_axis_tvalid <= 0;
-                else
-                    fifo_rden <= 1;
-            end
-            else if (fifo_rden == 1)
-                fifo_rden <= 1;
-            m_axis_tdata <= m_dout[m_addr];
-            m_axis_tuser <= fifo_dout[35:32];
-            m_addr <= m_addr + 1;
         end
     end
 endmodule
--- hdl/sim/interleaver_tb.v
+++ hdl/sim/interleaver_tb.v
@@ -1,5 +1,88 @@
 `timescale 1ns / 1ps
 
+`include "ieee80211_defs.v"
+
+`define INPUT_VECTOR  "encoded_data_bits.txt"
+`define OUTPUT_VECTOR "interleaved_data_bits.txt"
+
 module interleaver_tb;
 
+    `include "axis_tasks.v"
+
+    localparam WIDTH = 8;
+
+    localparam SEND_WIDTH = WIDTH;
+    localparam RECV_WIDTH = WIDTH;
+
+    localparam CLOCKPERIOD = 100;
+    localparam DATA_COUNT = 24;
+
+    reg [WIDTH-1:0] data_input [DATA_COUNT-1:0];
+    reg [WIDTH-1:0] data_output [DATA_COUNT-1:0];
+
+    reg clk = 1;
+    reg rst;
+
+    reg [WIDTH-1:0] i_tdata;
+    reg [WIDTH/2-1:0] i_tuser;
+    reg i_tvalid;
+    wire i_tready;
+    reg i_tlast;
+
+    wire [WIDTH-1:0] o_tdata;
+    wire [WIDTH/2-1:0] o_tuser;
+    wire o_tvalid;
+    reg o_tready;
+    wire o_tlast;
+
+    interleaver dut(
+        .aclk(clk),
+        .aresetn(rst),
+        .s_axis_tdata(i_tdata),
+        .s_axis_tvalid(i_tvalid),
+        .s_axis_tready(i_tready),
+        .s_axis_tuser(i_tuser),
+        .s_axis_tlast(i_tlast),
+        .m_axis_tdata(o_tdata),
+        .m_axis_tvalid(o_tvalid),
+        .m_axis_tuser(o_tuser),
+        .m_axis_tready(o_tready),
+        .m_axis_tlast(o_tlast)
+    );
+
+    always #(CLOCKPERIOD/2) clk <= ~clk;
+
+    initial begin
+        $display("Loading test vectors...");
+        $readmemb(`INPUT_VECTOR, data_input, 0, DATA_COUNT-1);
+        $readmemb(`OUTPUT_VECTOR, data_output, 0, DATA_COUNT-1);
+        $dumpfile("interleaver.vcd");
+        $dumpvars;
+    end
+
+    integer i;
+    initial begin
+        $display("Start...");
+        // Initialize and reset
+        i_tuser = 0;
+        i_tvalid = 0;
+        i_tlast = 0;
+        o_tready = 0;
+        reset();
+
+        $display("Starting DATA interleaving test...");
+        i_tuser = `RATE_36M;
+        for (i = 0; i < DATA_COUNT; i = i + 1) begin
+            send(data_input[i]);
+            if (o_tdata != data_output[i]) begin
+                $display("Failed DATA interleaving test %1d.", i);
+                $display("EXP: %b", data_output[i]);
+                $display("GOT: %b", o_tdata);
+                $finish;
+            end
+        end
+
+        $display("All tests succeeded.");
+        #(10*CLOCKPERIOD) $finish;
+    end
 endmodule
